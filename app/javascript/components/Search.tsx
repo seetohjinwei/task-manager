62979c8e2b622977f123e5de274229170a5a3e77
Jin Wei <seetohjinwei@gmail.com>
2021-12-15 19:02:02 +0800

    Added working search

--- app/javascript/components/Search.tsx
+++ app/javascript/components/Search.tsx
@@ -9,7 +9,40 @@ const Search = ({
   searchProps: ISearch;
   setSearchProps: React.Dispatch<React.SetStateAction<ISearch>>;
 }) => {
-  return <div>This is the search form. This is the search string: {searchProps.searchString}.</div>;
+  const handleOnChangeCheckbox: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSearchProps({ ...searchProps, [event.target.name]: event.target.checked });
+    // console.log(searchProps);
+  };
+  const handleOnChangeString: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSearchProps({ ...searchProps, [event.target.name]: event.target.value });
+    // console.log(searchProps);
+  };
+
+  return (
+    <form>
+      <input
+        type="checkbox"
+        name="displayDone"
+        checked={searchProps.displayDone}
+        onChange={handleOnChangeCheckbox}
+      />
+      {"Show Finished Tasks"}
+      <input
+        type="checkbox"
+        name="strictSearch"
+        checked={searchProps.strictSearch}
+        onChange={handleOnChangeCheckbox}
+      />
+      {"Match All Search Terms"}
+      <input
+        type="text"
+        name="searchString"
+        value={searchProps.searchString}
+        onChange={handleOnChangeString}
+      />
+    </form>
+  );
+  // return <div>This is the search form. This is the search string: {searchProps.searchString}.</div>;
 };
 
 export default Search;
--- app/javascript/components/Tasks.tsx
+++ app/javascript/components/Tasks.tsx
@@ -7,21 +7,28 @@ import React from "react";
 // TODO: exception for empty searchString
 
 const Tasks = ({ tasks, searchProps }: { tasks: ITask[]; searchProps: ISearch }) => {
+  // console.log(searchProps);
   const searchString: string = searchProps.searchString;
+  // console.log(searchString);
   const searchTerms: string[] = searchString.split(" ");
   function render(task: ITask, index: number) {
     // empty string is trivially included in every other string
     // can change to .every() if decide to match ALL search terms
     // might make that an option (strict search -- match all terms)
     // "searchOptions maybe" -- include in InterfaceSearch
-    const toRender: boolean = searchTerms.some((searchParam) => {
+    const matchParam = (searchParam: string): boolean => {
       const lowerCaseSearchParam: string = searchParam.toLowerCase();
       return (
-        (searchParam.startsWith("#") && task.tags.includes(searchParam.slice(1))) ||
+        (searchParam.startsWith("#") &&
+          task.tags.some((tag) => tag.includes(searchParam.slice(1)))) ||
         task.name.toLowerCase().includes(lowerCaseSearchParam) ||
         task.description.toLowerCase().includes(lowerCaseSearchParam)
       );
-    });
+    };
+    const toRender: boolean = searchProps.strictSearch
+      ? searchTerms.every(matchParam)
+      : searchTerms.some(matchParam);
+    // const toRender: boolean = searchTerms.some(verify);
     return toRender ? <Task key={index} {...task} /> : null;
   }
   return <div>{tasks.map(render)}</div>;
